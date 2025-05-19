37781357ab4f9b5bb70baaab1d2709b4d2e8fbef
Khazeeva Veronika <nikahazeeva@yandex.ru>
2025-05-20 00:00:03 +0300

    vesion 1

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,57 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from posts.models import Comment, Post, Follow, Group
+from django.contrib.auth import get_user_model
 
 
-from posts.models import Comment, Post
+User = get_user_model()
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
-
-    class Meta:
-        fields = '__all__'
-        model = Post
-
-
-class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        read_only=True,
+        slug_field='username'
     )
 
     class Meta:
+        model = Post
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True,
+                                          slug_field='username')
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
         model = Comment
+        fields = ['id', 'author', 'post', 'text', 'created']
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message='Вы уже оформили подписку на этого пользователя'
+            )
+        ]
+
+    def validate_following(self, value):
+        request = self.context.get('request')
+        if request and value == request.user:
+            raise serializers.ValidationError(
+                'Нельзя оформить подписку на самого себя!')
+        return value
--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import Group, Post, Comment, Follow
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'description')
+    search_fields = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author', 'group', 'image')
+    list_filter = ('pub_date', 'author', 'group')
+    search_fields = ('text', 'author__username', 'group__title')
+    date_hierarchy = 'pub_date'
+    raw_id_fields = ('author', 'group')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'text', 'created')
+    list_filter = ('created', 'author', 'post')
+    search_fields = ('text', 'author__username', 'post__text')
+    date_hierarchy = 'created'
+    raw_id_fields = ('author', 'post')
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following')
+    list_filter = ('user', 'following')
+    search_fields = ('user__username', 'following__username')
+    raw_id_fields = ('user', 'following')
